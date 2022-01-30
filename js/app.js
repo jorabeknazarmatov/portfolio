5a532d51f47cd16d4aae341541863818e384e690
Turabek <admiralno2@gmail.com>
2022-01-31 00:58:25 +0500

    Desktop versiya tayyor bo'lishiga faqat Project section qoldi.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger')
 const brgSpan = document.querySelectorAll('.burger span')
 const nav = document.querySelector('nav')
+const welcomeImg = document.querySelector('.welcome_img img')
+const about = document.querySelector('.about_cv')
 
 burger.addEventListener('click',()=>{
 	brgSpan[0].classList.toggle('brg_one')
@@ -8,3 +10,8 @@ burger.addEventListener('click',()=>{
 	brgSpan[2].classList.toggle('brg_thee')
 	nav.classList.toggle('nav_hidden')
 })
+if (screen.width>321) {
+	nav.classList.toggle('nav_hidden')
+	welcomeImg.getAttributeNode('src').value = 'images/image-geometry_1.svg';
+
+}
